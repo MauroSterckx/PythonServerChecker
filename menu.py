4a6e109a96f83d4a06cdf70d9919e1b56461513b
MauroSterckx <maurosterckx@gmail.com>
2023-11-05 14:01:06 +0100

    Add removeServer method

--- menu.py
+++ menu.py
@@ -2,20 +2,41 @@ import json
 
 
 def menu():
-    with open('ips.json', 'r') as f:
+    ...
+
+
+def addServer():
+    with open("ips.json", "r") as f:
         data = json.load(f)
         ip_data = data["ips"]
         print("Volgende ip-adressen worden gecheckt:")
         for element in ip_data:
             print(f"+ {element}")
         print("\n\n")
-    
+
     while True:
         print("type 'stop' om te stoppen met ip's ingeven")
         newIP = input("Geef nieuw ip >")
         if newIP != "stop":
             ip_data.append(newIP)
         else:
-            with open("ips.json" , "w") as f:
-                json.dump({"ips" : ip_data}, f, indent=4)
+            with open("ips.json", "w") as f:
+                json.dump({"ips": ip_data}, f, indent=4)
             break
+
+
+def removeServer():
+    with open("ips.json", "r") as f:
+        data = json.load(f)
+        ip_data = data["ips"]
+        print("Volgende ip-adressen worden gecheckt:")
+        for index, element in enumerate(ip_data):
+            print(f"[{index}] {element}")
+        print("\n\n")
+
+    while True:
+        print("type 'stop' om te stoppen met ip's te verwijderen")
+        remIp = input("Geef index van ip > ")
+
+
+removeServer()
